5b30b4607d6f9c281a69dd0727dc07ff8a0ed947
Yurii Khomin <yurii.khomin@gmail.com>
2021-09-27 22:53:08 +0300

    hw-2-fin

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,108 @@
 const express = require('express')
 const router = express.Router()
+const validate = require('../api/validation.js')
+const Contacts = require('../../model/index.js')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await Contacts.listContacts()
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contacts,
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  try {
+    const contact = await Contacts.getContactById(contactId)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        }
+      })
+    } else {
+      return res.json({
+        status: 'error',
+        code: 404,
+        data: 'Not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validate.createContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body)
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        contact,
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  try {
+    const contact = await Contacts.removeContact(contactId)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        }
+      })
+    } else {
+      return res.json({
+        status: 'error',
+        code: 404,
+        data: 'Not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  try {
+    const contact = await Contacts.updateContact(contactId, req.body)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        }
+      })
+    } else {
+      return res.json({
+        status: 'error',
+        code: 404,
+        data: 'Not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
---
+++ routes/api/validation.js
@@ -0,0 +1,37 @@
+const Joi = require('joi')
+
+const schemaCreateContact = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)
+    .required(),
+})
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().min(3).max(20).optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .pattern(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)
+    .optional(),
+}).min(1)
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj)
+  if (error) {
+    const [{ message }] = error.details
+    return next({
+      status: 400,
+      message: `Filed: ${message.replace(/"/g, '')}`,
+    })
+  }
+  next()
+}
+
+module.exports.createContact = (req, res, next) => {
+  return validate(schemaCreateContact, req.body, next)
+}
+
+module.exports.updateContact = (req, res, next) => {
+  return validate(schemaUpdateContact, req.body, next)
+}

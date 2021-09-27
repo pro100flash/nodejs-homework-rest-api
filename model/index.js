fb30c7497cabccc1ed317c5a178be182bc694309
Yurii Khomin <yurii.khomin@gmail.com>
2021-09-28 01:17:07 +0300

    hw-02-fin

--- model/index.js
+++ model/index.js
@@ -1,15 +1,77 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, '/contacts.json')
+console.log(contactsPath)
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8')
+    return JSON.parse(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts()
+    const contact = contacts.find(({ id }) => id.toString() === contactId)
+    return contact
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const deletedContact = getContactById(contactId)
+    if (!deletedContact) return
 
-const updateContact = async (contactId, body) => {}
+    const contacts = await listContacts()
+    const filteredContacts = contacts.filter((contact) => {
+      return contact.id !== contactId
+    })
+
+    const stringifiedArray = JSON.stringify(filteredContacts, null, 2)
+    await fs.writeFile(contactsPath, stringifiedArray, 'utf8')
+    return deletedContact
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts()
+    const newContact = { id: uuidv4(), ...body }
+    const contactsArray = [...contacts, newContact]
+
+    const stringifiedArray = JSON.stringify(contactsArray, null, 2)
+
+    await fs.writeFile(contactsPath, stringifiedArray, 'utf8')
+    return newContact
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts()
+    const index = contacts.findIndex(({ id }) => id.toString() === contactId)
+    if (index === -1) return
+    contacts[index] = { ...contacts[index], ...body }
+
+    const stringifiedArray = JSON.stringify(contacts, null, 2)
+
+    await fs.writeFile(contactsPath, stringifiedArray, 'utf8')
+    return contacts[index]
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   listContacts,
